b08b46cfd86826e166fbcf91beafb69573e71ae8
quangdvgugroup <quangdv@gu-group.com>
2025-11-12 17:01:50 +0700

    fix-build-error

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 // config-overrides.js
-const { override, addWebpackAlias } = require('customize-cra');
+const { override, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = override(
@@ -7,5 +8,11 @@ module.exports = override(
   // For example, adding a webpack alias for cleaner imports:
   addWebpackAlias({
     '@': path.resolve(__dirname, 'src'),
-  })
+  }),
+  // Ignore React Native modules that are not needed for web builds
+  addWebpackPlugin(
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+    })
+  )
 );
